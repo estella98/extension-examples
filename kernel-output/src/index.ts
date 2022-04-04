88595bc8c535ccc96053847457e8ddd3752b6794
Estella Liu <estellaliu@Estellas-MacBook-Pro.local>
2022-04-04 01:21:33 -0400

    clean

--- kernel-output/src/index.ts
+++ kernel-output/src/index.ts
@@ -10,7 +10,6 @@ import { ILauncher } from '@jupyterlab/launcher';
 import { ITranslator } from '@jupyterlab/translation';
 import { INotebookTracker } from '@jupyterlab/notebook';
 import { IRenderMimeRegistry } from '@jupyterlab/rendermime';
-import { ICodeCellModel } from '@jupyterlab/cells';
 import { ExamplePanel } from './panel';
 
 import { DocumentRegistry } from '@jupyterlab/docregistry';
@@ -24,15 +23,6 @@ import {
   NotebookPanel,
   INotebookModel,
 } from '@jupyterlab/notebook';
-
-/**
- * The command IDs used by the console plugin.
- */
-namespace CommandIDs {
-  export const create = 'kernel-output:create';
-
-  export const execute = 'kernel-output:execute';
-}
 
 /**
  * Initialization data for the extension.
@@ -68,72 +58,6 @@ function activate(
   notebookTracker: INotebookTracker,
   launcher: ILauncher | null
 ): void {
-  const manager = app.serviceManager;
-  const { commands, shell } = app;
-  const category = 'Extension Examples';
-  const trans = translator.load('jupyterlab');
-
-  let panel: ExamplePanel;
-
-  /**
-   * Creates a example panel.
-   *
-   * @returns The panel
-   */
-  async function createPanel(): Promise<ExamplePanel> {
-    panel = new ExamplePanel(
-      app,
-      notebookTracker,
-      manager,
-      rendermime,
-      translator
-    );
-    shell.add(panel, 'main');
-    return panel;
-  }
-
-  // add commands to registry
-  commands.addCommand(CommandIDs.create, {
-    label: trans.__('Open the Presentation Kernel'),
-    caption: trans.__('Open the Presentation Kernel'),
-    execute: createPanel,
-  });
-
-  // commands.addCommand(CommandIDs.execute, {
-  //   label: trans.__('Contact Kernel and Execute Code'),
-  //   caption: trans.__('Contact Kernel and Execute Code'),
-  //   execute: async () => {
-  //     // Create the panel if it does not exist
-  //     if (!panel) {
-  //       await createPanel();
-  //     }
-  //     // Prompt the user about the statement to be executed
-  //     const input = await InputDialog.getText({
-  //       title: trans.__('Code to execute'),
-  //       okLabel: trans.__('Execute'),
-  //       placeholder: trans.__('Statement to execute'),
-  //     });
-  //     // Execute the statement
-  //     if (input.button.accept) {
-  //       const code = input.value;
-  //       panel.execute(code);
-  //     }
-  //   },
-  // });
-
-  // add items in command palette and menu
-  [CommandIDs.create, CommandIDs.execute].forEach((command) => {
-    palette.addItem({ command, category });
-  });
-
-  // Add launcher
-  if (launcher) {
-    launcher.add({
-      command: CommandIDs.create,
-      category: category,
-    });
-  }
-
   app.docRegistry.addWidgetExtension(
     'Notebook',
     new ButtonExtension2(
@@ -147,15 +71,10 @@ function activate(
   );
 }
 
-const plugin: JupyterFrontEndPlugin<void> = {
-  activate,
-  id: 'toolbar-button',
-  autoStart: true,
-};
-
 // add new change experiments
 export class ButtonExtension2
-  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>
+{
   /**
    * Create a new extension for the notebook panel widget.
    
@@ -220,75 +139,4 @@ export class ButtonExtension2
   }
 }
 
-const copyOutputPlugin: JupyterFrontEndPlugin<void> = {
-  id: '@jupyterlab/notebook-extension:copy-output',
-  activate: activateCopyOutput,
-  requires: [ITranslator, INotebookTracker],
-  autoStart: true,
-};
-
-function activateCopyOutput(
-  app: JupyterFrontEnd,
-  notebookTracker: INotebookTracker
-) {
-  console.log('activate Copy Output is called');
-  if (notebookTracker.activeCell) {
-    let codeCell = notebookTracker.activeCell.model as ICodeCellModel;
-    console.log(codeCell);
-    let outputs = codeCell.outputs;
-    console.log(outputs);
-    for (let i = 0; i < outputs.length; i++) {
-      // IOutputModel
-      const outputModel = outputs.get(i);
-      const outputData = outputModel.data;
-      console.log('\t\tdata is', outputData);
-      // also has `outputModel.executionCount` and `outputModel.metadata`
-    }
-  }
-
-  notebookTracker.widgetUpdated.connect((tracker, panel) => {
-    console.log('widget is updated');
-    console.log(panel.model.cells);
-    let notebook = panel.content;
-    const notebookModel = notebook.model;
-    console.log('notebook model is');
-    console.log(notebookModel);
-    notebookModel.cells.changed.connect((_, change) => {
-      if (change.type != 'add') {
-        return;
-      }
-      for (const cellModel of change.newValues) {
-        // ensure we have CodeCellModel
-        if (cellModel.type != 'code') {
-          return;
-        }
-        // IOutputAreaModel
-        let outputs = (cellModel as ICodeCellModel).outputs;
-        console.log('outputs are');
-        console.log(outputs);
-        if (!outputs) {
-          continue;
-        }
-        outputs.changed.connect(() => {
-          console.log(
-            'Outputs of the cell',
-            cellModel.id,
-            'in',
-            notebook.title.label,
-            'changed:'
-          );
-          console.log('\tThere are now', outputs.length, 'outputs:');
-          for (let i = 0; i < outputs.length; i++) {
-            // IOutputModel
-            const outputModel = outputs.get(i);
-            console.log('\t\t', outputModel.data);
-            // also has `outputModel.executionCount` and `outputModel.metadata`
-          }
-        });
-      }
-    });
-  });
-}
-
 export default extension;
-export { plugin, copyOutputPlugin };
